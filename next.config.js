b115c6e441f7b2a6253d074693ae24d1d6d97c43
Tom Bray <tom@tombray.com>
2022-12-01 08:40:44 -0800

    enable source maps

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  productionBrowserSourceMaps: true,
   reactStrictMode: true,
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
